dfa36569c13c16aea585c81974b50dc9a7bfab53
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-09-27 12:59:49 -0300

    Teste

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          FUNCIONOU, SEUS FRANGOS!!
+          FUNCIONOU, SEUS LINDOS!!
         </p>
       </header>
     </div>
